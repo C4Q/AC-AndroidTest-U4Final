1acf52afe198aa1be900a535f558de28c35d73c9
Justice Ogbonna <ejo36@drexel.edu>
2018-01-08 20:12:53 -0500

    add networking solution

--- app/src/main/java/nyc/c4q/androidtest_unit4final/MainActivity.java
+++ app/src/main/java/nyc/c4q/androidtest_unit4final/MainActivity.java
@@ -1,10 +1,22 @@
 package nyc.c4q.androidtest_unit4final;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ColorAdapter(colorsList, colorDict);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        DataDownloader dd = new DataDownloader();
+        dd.execute("https://raw.githubusercontent.com/operable/cog/master/priv/css-color-names.json");
     }
 
 
@@ -41,5 +55,51 @@ public class MainActivity extends AppCompatActivity {
         List<String> colors = new ArrayList<>();
         for(String n: names) colors.add(n);
         return colors;
+    }
+
+    class DataDownloader extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadData(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            Type collectionType = new TypeToken<HashMap<String, String>>() {}.getType();
+            Gson gs = new Gson();
+            HashMap<String, String> res = gs.fromJson(new StringReader(data), collectionType);
+            colorDict.putAll(res);
+            adapter.notifyDataSetChanged();
+        }
+
+
+        private String downloadData(String urlString) throws IOException {
+            InputStream is = null;
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+
+                is = conn.getInputStream();
+                BufferedReader r = new BufferedReader(new InputStreamReader(is));
+                StringBuilder total = new StringBuilder();
+                String line;
+                while ((line = r.readLine()) != null) {
+                    total.append(line);
+                }
+                return new String(total);
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
     }
 }
